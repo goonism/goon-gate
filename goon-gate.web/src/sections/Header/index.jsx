c3ac007ee0a17785054f8d6998ec2c871d2e738a
Joe Johans <lgvichy@gmail.com>
2017-11-04 23:24:27 -0400

    Add a silly header

--- goon-gate.web/src/sections/Header/index.jsx
+++ goon-gate.web/src/sections/Header/index.jsx
@@ -2,17 +2,50 @@ import React from 'react'
 
 import {Link} from 'react-router-dom';
 
+import styled from 'styled-components';
+
+const StyledHeader = styled.header`
+	display: fixed;
+	top: 0;
+	height: 10vh;
+	width: 100vw;
+`
+
+const StyledNav = styled.nav`
+	/* This renders the buttons above... Edit me! */
+	display: flex;
+
+`
+
+const StyledLink = styled(Link)`
+	display: flex;
+	justify-content: center;
+	align-items: center;
+
+	transition: 0.5s;
+
+	color: palevioletred;
+	font-size: 1em;
+	margin: 1em;
+	padding: 0.25em 1em;
+	border: 2px solid palevioletred;
+	border-radius: 3px;
+	text-decoration: none;
+	&:hover {
+		background: palevioletred;
+		color: white;
+	}
+`
+
 export default class Header extends React.Component {
 	render() {
 		return(
-			<header className="Header">
-				<nav>
-					<ul>
-						<li><Link to='/'>Home</Link></li>
-						<li><Link to='/diffy'>Diffy</Link></li>
-					</ul>
-				</nav>
-			</header>
+			<StyledHeader>
+				<StyledNav>
+					<StyledLink to='/'>Home</StyledLink>
+					<StyledLink to='/diffy'>Diffy</StyledLink>
+				</StyledNav>
+			</StyledHeader>
 		)
 	}
 }
