5adbeea5046cf0a428493862cb81d520a70bb891
Stefan Aleksic <stefan@stefanaleksic.com>
2017-11-05 22:33:46 -0800

    Fix up code and add flow.

--- goon-gate.web/src/api.jsx
+++ goon-gate.web/src/api.jsx
@@ -3,16 +3,6 @@ import IPFS from "ipfs";
 
 export const customHistory = createBrowserHistory();
 
-export const DAYS = [
-	"Sunday",
-	"Monday",
-	"Tuesday",
-	"Wednesday",
-	"Thursday",
-	"Friday",
-	"Saturday"
-];
-
-export const repo = "diffy-13234234";
+export const repo = "diffy-13234234"
 
 export const ipfsNode = new IPFS({ repo });
